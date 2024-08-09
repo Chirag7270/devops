dc05d21a572d29167c59dd7ff340b356d7fbf16f
[Chirag Choudhary] <chiragchoudhary170@gmail.com>
2024-08-09 07:04:05 +0530

    This is second commit

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Chirag");
+
+        int a = 10;
+        int b = 27;
+        int sum = a+b;
+        System.out.println("The value of sum is: "+sum);
     }
 }
